2b9bf5b6abc5eeebc3da495670a1e5ead0b62745
MV <68259555+MaddieVince@users.noreply.github.com>
2020-09-19 11:51:13 +1000

    Updated default values for user model

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -7,10 +7,10 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=200)
     company = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
-    user_bio = models.TextField()
-    bio_pic = models.URLField()
-    date_joined = models.DateField()
-    project_owner = models.BooleanField()
+    user_bio = models.TextField(default="")
+    bio_pic = models.URLField(default="")
+    date_joined = models.DateField(default=datetime.datetime.now())
+    project_owner = models.BooleanField(default=True)
 
     def __str__(self):
         return self.username
